5acfbc86bab84563429ae05a52ba376312e5ad8b
mungyu <mungyu83@gmail.com>
2023-07-31 01:51:12 +0900

    홈 스크린 - 하단 모달 띄우기(showModalBottomSheet).

---
+++ lib/screens/components/small_sub_text.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SmallSubText extends StatelessWidget {
+
+  final String text;
+
+  const SmallSubText({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: Colors.grey,
+        fontSize: 12.0
+      ),
+    );
+  }
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:netflix_ui_flutter/screens/components/play_button.dart';
+import 'package:netflix_ui_flutter/screens/components/small_sub_text.dart';
 import 'package:netflix_ui_flutter/screens/home/components/rank_poster.dart';
 
 import '../components/label_icon.dart';
@@ -126,32 +127,42 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
                 SliverToBoxAdapter(
-                  child: Container(
-                    height: (appSize.height * 0.6),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          "오늘 한국에서 콘텐츠 순위 1위",
-                          style: TextStyle(fontSize: 16.0),
-                        ),
-                        SizedBox(height: 20.0),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            LabelIcon(
-                              icon: FontAwesomeIcons.plus,
-                              label: "내가 찜한 콘텐츠",
-                            ),
-                            PlayButton(width: 80.0),
-                            LabelIcon(
-                              icon: Icons.info_outline,
-                              label: "정보"
-                            )
-                          ],
-                        ),
-                        SizedBox(height: 30.0)
-                      ],
+                  child: GestureDetector(
+                    behavior: HitTestBehavior.translucent, // GestureDetector는 기본적으로 빈공간이 아닌 위젯을 터치했을때 동작함.
+                                                           // 빈공간을 터치해도 영역안이라면 이벤트가 일어나도록 하기위한 옵션.
+                    onTap: () {
+                      showModalBottomSheet( // BottomSheet 모달 띄우기
+                        context: context,
+                        builder: _buildInfoButtomSheet,
+                      );
+                    },
+                    child: Container(
+                      height: (appSize.height * 0.6),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            "오늘 한국에서 콘텐츠 순위 1위",
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                          SizedBox(height: 20.0),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              LabelIcon(
+                                icon: FontAwesomeIcons.plus,
+                                label: "내가 찜한 콘텐츠",
+                              ),
+                              PlayButton(width: 80.0),
+                              LabelIcon(
+                                icon: Icons.info_outline,
+                                label: "정보"
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 30.0)
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -236,4 +247,122 @@ class _HomeScreenState extends State<HomeScreen> {
       ],
     );
   }
+
+  Widget _buildInfoButtomSheet(BuildContext context) {
+      return Wrap( // BottomSheet는 기본적으로 화면의 절반정도를 차지함. 만약 안의 내용만큼 크기를 차지하도록 줄이려면 Wrap으로 감싸주면 됨.
+        children: [
+          Stack(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Color(0xFF2B2B2B),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10.0),
+                    topRight: Radius.circular(10.0)
+                  )
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(5.0),
+                            child: Image(
+                              image: AssetImage("assets/images/big_buck_bunny_poster.jpg"),
+                              width: 100.0,
+                            ),
+                          ),
+                          SizedBox(width: 10.0),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "빅 벅 머니",
+                                  style: TextStyle(fontSize: 18.0),
+                                ),
+                                Row(
+                                  children: [
+                                    SmallSubText(text: "2008"),
+                                    SizedBox(width: 10.0),
+                                    SmallSubText(text: "15+"),
+                                    SizedBox(width: 10.0),
+                                    SmallSubText(text: "시즌 1개"),
+                                  ],
+                                ),
+                                SizedBox(height: 8.0),
+                                Text(
+                                  "버니가 좋아하는 나비들 중 2마리가 죽고 "
+                                  "버니 자신에게 공격이 오자 "
+                                  "버니는 온순한 자연을 뒤로 하고 "
+                                  "2마리의 나비로 인해 복수할 계획들을 치밀히 세운다."
+                                )
+                              ],
+                            )
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 10.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          PlayButton(width: 160.0),
+                          LabelIcon(
+                            icon: FontAwesomeIcons.download,
+                            label: "저장",
+                            style: TextStyle(
+                              fontSize: 12.0,
+                              color: Colors.grey
+                            ),
+                          )
+                        ],
+                      ),
+                      Divider(),
+                      GestureDetector(
+                        behavior: HitTestBehavior.translucent,
+                        onTap: () {},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(Icons.info_outline),
+                                SizedBox(width: 10.0),
+                                Text("회차 및 상세정보")
+                              ],
+                            ),
+                            Icon(FontAwesomeIcons.chevronRight, size: 16.0)
+                          ],
+                        ),
+                      )
+                    ]
+                  ),
+                ),
+              ),
+              Positioned(
+                right: 10.0,
+                top: 10.0,
+                child: GestureDetector( // BottomSheet는 자신의 외부 영역을 터치하면 닫힘. 만약 닫는 이벤트를 직접 지정하고 싶을때는 Navigator.pop 이용 가능
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    width: 30.0,
+                    height: 30.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25.0),
+                      color: Color(0xFF525252)
+                    ),
+                    child: Icon(FontAwesomeIcons.times), // X 모양 버튼
+                  ),
+                )
+              )
+            ],
+          ),
+        ],
+      );
+    }
 }
