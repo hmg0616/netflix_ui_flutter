a42558a87344b5c9c7b56af5be2545321da58f25
mungyu <mungyu83@gmail.com>
2023-08-01 22:22:23 +0900

    상세정보 스크린 - 동영상 플레이어

--- lib/screens/detail/detail_screen.dart
+++ lib/screens/detail/detail_screen.dart
@@ -1,5 +1,7 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:video_player/video_player.dart';
 
 class DetailScreen extends StatefulWidget {
 
@@ -10,6 +12,45 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
+
+  List<String> posters =  [
+    "assets/images/big_buck_bunny_poster.jpg",
+    "assets/images/les_miserables_poster.jpg",
+    "assets/images/minari_poster.jpg",
+    "assets/images/the_book_of_fish_poster.jpg",
+  ];
+
+  late VideoPlayerController _videoPlayerController;
+  ChewieController? _chewieController; // 영상 재생 위젯
+
+  Future<void> initializePlayer() async { // 비동기로 데이터를 불러오기위해 Future, async, await 사용
+    _videoPlayerController = VideoPlayerController.network(
+        'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4'
+    );
+    await Future.wait([_videoPlayerController.initialize()]); // 비디오 다운로드가 완료될 때까지 대기
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController,
+      autoPlay: true,
+    );
+
+    setState(() {
+
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializePlayer();
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    _chewieController?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,8 +71,22 @@ class _DetailScreenState extends State<DetailScreen> {
           SizedBox(width: 15.0)
         ],
       ),
-      body: Center(
-        child: Text("DetailScreen"),
+      body: Column(
+        children: [
+          Container(
+            height: 230.0,
+            child: _chewieController != null && _chewieController!.videoPlayerController.value.isInitialized
+                ? Chewie(controller: _chewieController!)
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      CircularProgressIndicator(),
+                      SizedBox(height: 20),
+                      Text('Loading')
+                    ],
+                  ),
+          )
+        ],
       ),
     );
   }
